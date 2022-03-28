d52a7808f589301e9f74c600bcce53dea8617940
maikytech <maikytech1307@gmail.com>
2022-03-28 17:19:55 -0500

    Start Project

---
+++ FacebookCoverSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FacebookCoverSwiftUI
+//
+//  Created by Maiqui Cedeño on 28/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FacebookCoverSwiftUI/FacebookCoverSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookCoverSwiftUIApp.swift
+//  FacebookCoverSwiftUI
+//
+//  Created by Maiqui Cedeño on 28/03/22.
+//
+
+import SwiftUI
+
+@main
+struct FacebookCoverSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
