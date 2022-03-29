e658c2aa950eca9ac0fa4befe174411e84d75764
maikytech <maikytech1307@gmail.com>
2022-03-29 17:58:39 -0500

    Update UI

--- FacebookCoverSwiftUI/ContentView.swift
+++ FacebookCoverSwiftUI/ContentView.swift
@@ -14,10 +14,33 @@ struct Settings {
 }
 
 struct ContentView: View {
+    @State var emailValue: String = ""
+    
     var body: some View {
-        VStack {
-            Image("logo")
+        ScrollView {
+            VStack {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 250, height: 54)
+                    .clipped()
+                    .scaledToFill()
+                Text("Sign In")
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(10)
+                
+                TextField("email", text: $emailValue)
+                    .padding(.all, 10)
+                    .foregroundColor(.black)
+                    .accentColor(.pink)
+                    .background(.white)
+                    .cornerRadius(25)
+            }
+            .padding(.top, 50)
         }
+        .background(Settings.facebookColor)
+        .edgesIgnoringSafeArea(Edge.Set.all)
     }
 }
 
